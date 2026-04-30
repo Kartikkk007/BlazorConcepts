4471cef6d6700a51adf119e1b1619c79fb3d4011
Kartik Sharma <kartiksharma4418@gmail.com>
2026-05-01 02:01:41 +0530

    data annotations

--- BlazorConcepts/Models/UserProfile.cs
+++ BlazorConcepts/Models/UserProfile.cs
@@ -5,12 +5,16 @@ namespace MyFirstBlazorApp.Models
     public class UserProfile
     {
         [Required(ErrorMessage ="full name is required")]
+        [StringLength(30, ErrorMessage = "full name cannot be more than 30 characters")]
+
         public string? FullName { get; set; }
         
         [Required(ErrorMessage = "email is required")]
+        [EmailAddress(ErrorMessage = "invalid email format")]
         public string? Email { get; set; }
        
         [Required(ErrorMessage = "age is required")]
+        [Range(10, 70, ErrorMessage = "age must be between 10 and 70")]
         public int? Age { get; set; }
 
 
