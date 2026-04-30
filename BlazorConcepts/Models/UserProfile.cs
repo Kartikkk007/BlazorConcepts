53e3a8814f5fd75a7ed92fe4dc5e8c0af3528c97
Kartik Sharma <kartiksharma4418@gmail.com>
2026-04-30 18:46:06 +0530

    event handling , navigation and user forms

---
+++ BlazorConcepts/Models/UserProfile.cs
@@ -0,0 +1,14 @@
+﻿namespace MyFirstBlazorApp.Models
+{
+    public class UserProfile
+    {
+        public string? FullName { get; set; }
+
+        public string? Email { get; set; }
+
+        public int Age { get; set; }
+
+
+
+    }
+}
