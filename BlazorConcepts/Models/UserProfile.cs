b92b0c946b2020a96fac5d484c05843f54124019
Kartik Sharma <kartiksharma4418@gmail.com>
2026-05-01 01:50:45 +0530

    data annotations

--- BlazorConcepts/Models/UserProfile.cs
+++ BlazorConcepts/Models/UserProfile.cs
@@ -1,12 +1,17 @@
-﻿namespace MyFirstBlazorApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyFirstBlazorApp.Models
 {
     public class UserProfile
     {
+        [Required(ErrorMessage ="full name is required")]
         public string? FullName { get; set; }
-
+        
+        [Required(ErrorMessage = "email is required")]
         public string? Email { get; set; }
-
-        public int Age { get; set; }
+       
+        [Required(ErrorMessage = "age is required")]
+        public int? Age { get; set; }
 
 
 
